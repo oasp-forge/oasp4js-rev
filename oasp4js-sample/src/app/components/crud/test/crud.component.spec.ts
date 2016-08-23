5ae78b9ed92ab39f0d7b6f4982273a8000995c22
Belda Perez <cristobal.belda-perez@capgemini.com>
2016-08-23 14:55:12 +0200

    crud.service.ts file test 2 (crud.component.spec commented) #15

--- oasp4js-sample/src/app/components/crud/test/crud.component.spec.ts
+++ oasp4js-sample/src/app/components/crud/test/crud.component.spec.ts
@@ -1,23 +1,48 @@
-import {
-  beforeEachProviders,
-  describe,
-  expect,
-  it,
-  inject
-} from '@angular/core/testing';
-import { CrudComponent } from '../view/Crud.component';
-import { CrudService } from '../service/Crud.service'
-
-beforeEachProviders(() => [CrudComponent]);
-
-describe('App: CrudComponent', () => {
-  it('should create the Component CRUD',
-      inject([CrudComponent], (crud: CrudComponent) => {
-    expect(crud).toBeTruthy();
-  }));
-
-  it('should have as message \'crud.component is working!\'',
-      inject([CrudComponent], (crud: CrudComponent) => {
-    expect(crud.message).toEqual('crud.component is working!');
-  }));
-});
+// import {CrudComponent} from '../view/Crud.component';
+// import {CrudService} from '../service/Crud.service';
+// // import {
+// //   beforeEachProviders,
+// //   beforeEach,
+// //   describe,
+// //   expect,
+// //   it,
+// //   inject,
+// //   TestComponentBuilder
+// // } from '@angular/core/testing';
+//
+// import {
+//   beforeEachProviders,
+//   beforeEach,
+//   describe,
+//   expect,
+//   it,
+//   inject,
+//   TestComponentBuilder
+// } from 'angular2/testing';
+//
+// describe('Component: CrudComponent', () => {
+//   let tcb;
+//
+//   //setup
+//   beforeEachProviders(() => [
+//     TestComponentBuilder,
+//     CrudComponent
+//   ]);
+//
+//   beforeEach(inject([TestComponentBuilder], _tcb => {
+//     tcb = _tcb
+//   }));
+//
+//   //specs
+//   it('should render `Hello World!`', done => {
+//     tcb.createAsync(CrudComponent).then(fixture => {
+//       let crud = fixture.componentInstance;
+//       let element = fixture.nativeElement;
+//       crud.name = 'World';
+//       fixture.detectChanges(); //trigger change detection
+//       expect(crud.message).toBe('Hello World!');
+//       done();
+//     })
+//     .catch(e => done.fail(e));
+//   });
+// })
