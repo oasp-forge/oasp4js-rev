1a08da0dd510952e3dcdfb55729f112baaeeb945
Belda Perez <cristobal.belda-perez@capgemini.com>
2016-08-24 15:12:15 +0200

    submit/cancel functionality integrated with new grid-table oasp component

---
+++ oasp4js-sample/src/app/test/details/details.service.spec.ts
@@ -0,0 +1,40 @@
+// import {CrudService} from '../../components/crud/service/Crud.service';
+// import {Table} from '../../models/table/Table.model'
+// import {Command} from '../../models/command/Command.model'
+// import {beforeEachProviders,beforeEach,describe,expect,it,inject} from '@angular/core/testing';
+// import {commandsList} from '../../resources/commands/Commands.resource'
+//
+// describe('DetailsService [SERVICE]: \n', () => {
+//   let service;
+//
+//   beforeEachProviders(() => [CrudService]);
+//
+//   beforeEach(inject([CrudService], s => {
+//     service = s;
+//   }));
+//
+//   it('[TEST_ERROR] DetailsService should have 2 attributes!', () => {
+//     // var count = 0;
+//     // for (var k in foo) {
+//     //     if (foo.hasOwnProperty(k)) {
+//     //        ++count;
+//     //     }
+//     // }
+//     expect(Object.prototype.toString.call(tables)).toBe('[object Array]');
+//   })
+//
+//   it('[TEST_ERROR] DetailsService should have 2 arrays!', () => {
+//     expect(Object.prototype.toString.call(tables)).toBe('[object Array]');
+//   })
+//
+//   it('[TEST_ERROR] Array TABLES CrudService should not be empty!', () => {
+//     expect(tables.length).toBeGreaterThan(0);
+//   })
+//
+//   it('[TEST_ERROR] items of TABLES should be instances of class Table!', () => {
+//     let index = Math.floor((Math.random() * tables.length) + 0);
+//     expect(tables[index] instanceof Table).toBe(true);
+//   })
+//
+//
+// })
