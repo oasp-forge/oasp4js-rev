aac65956ebf3a58baac78dbf5a55f0d9643bda23
Garcia Marin <roberto.garcia-marin@capgemini.com>
2016-08-29 10:03:56 +0200

    #14 routing Tables-Kitchen implemented

--- oasp4js-sample/src/app/oasp4js-sample.component.ts
+++ oasp4js-sample/src/app/oasp4js-sample.component.ts
@@ -3,12 +3,16 @@ import { Component } from '@angular/core';
 import { HeaderComponent} from './components/header/view/Header.component';
 import { LoginComponent} from './components/login/view/Login.component';
 import { CrudComponent } from './components/crud/view/Crud.component'
+import { KitchenComponent } from './components/kitchen/view/Kitchen.component'
 
 import { User } from './models/user/User.model'
 
 import {ROUTER_DIRECTIVES, Router, Routes} from "@angular/router";
 
 @Routes([
+    { path: '', component: CrudComponent },
+    { path: '/Tables', component: CrudComponent },
+    { path: '/Kitchen', component: KitchenComponent }
 ])
 
 @Component({
@@ -18,16 +22,22 @@ import {ROUTER_DIRECTIVES, Router, Routes} from "@angular/router";
   styleUrls: ['oasp4js-sample.component.css'],
   directives: [LoginComponent, HeaderComponent, CrudComponent, ROUTER_DIRECTIVES]
 })
+
+
 export class Oasp4jsSampleAppComponent{
   title = "oasp4js-sample works!";
   public login:boolean;
   public usuario: User = new User(0,"","");
 
+  constructor(private router: Router){}
+
   enviar(value){
     setTimeout( ()=>{this.login = value;},1);
+    this.router.navigate(['/Tables']);
   }
 
   logOut(){
     this.login = false;
+    this.router.navigate(['']);
   }
 }
--- oasp4js-sample/src/main.ts
+++ oasp4js-sample/src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 // import {bootstrap} from 'angular2/platform/browser'
 import { enableProdMode } from '@angular/core';
-import { APP_ROUTER_PROVIDERS } from './app.routes';
+import { ROUTER_PROVIDERS, ROUTER_DIRECTIVES } from '@angular/router';
 import { Oasp4jsSampleAppComponent, environment } from './app/';
 //import {disableDeprecatedForms, provideForms} from "@angular/forms";
 
@@ -9,7 +9,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(Oasp4jsSampleAppComponent/*,[
-  disableDeprecatedForms(),
-  provideForms()
-]*/);
+
+bootstrap(Oasp4jsSampleAppComponent, [ROUTER_PROVIDERS, ROUTER_DIRECTIVES])
+  .catch(err => console.error(err));
