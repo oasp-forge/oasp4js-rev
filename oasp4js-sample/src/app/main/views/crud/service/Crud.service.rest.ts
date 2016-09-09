e306fd3d361bfe314f74fe9245f27d8f41c45817
Belda Perez <cristobal.belda-perez@capgemini.com>
2016-09-09 08:33:25 +0200

    added BusinessOperations.ts

--- oasp4js-sample/src/app/main/views/crud/service/Crud.service.rest.ts
+++ oasp4js-sample/src/app/main/views/crud/service/Crud.service.rest.ts
@@ -1,50 +1,43 @@
 import { Injectable } from '@angular/core';
 import {Table} from '../../../models/table/Table.model'
 import {Command} from '../../../models/command/Command.model'
-//import { environment } from '../environment';
+import { BusinessOperations } from '../../../../main/BusinessOperations';
 import { Http, Response,Headers } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
-/*
-  Need to know how to do a http connection with the server
-*/
+
 @Injectable()
 export class CrudRestService {
-  serverPath:String =  'http://10.68.8.26:8081/oasp4j-sample-server/';
-  basePath:String = this.serverPath + 'services/rest/tablemanagement/v1';
-  productsPath:String = this.serverPath + 'services/rest/offermanagement/v1';
 
- constructor(private http:Http) { }
+  BO:BusinessOperations = new BusinessOperations();
+  constructor(private http:Http) { }
 
   getTables(){
-    return this.http.get(this.basePath + '/table/')
-                           .map(res =>  res.json())
+    return this.http.get(this.BO.tablesGET)
+                    .map(res =>  res.json())
   }
 
   getOffers(){
-    return this.http.get(this.productsPath + '/offer/')
-                           .map(res =>  res.json())
+    return this.http.get(this.BO.offersGET)
+                    .map(res =>  res.json())
   }
 
   applyFilters(filters){
-      return this.http.get(this.basePath + '/table/' + 10 + filters[0])
-                             .map(res =>  res.json())
+    return this.http.get(this.BO.tablesGET + 10 + filters[0])
+                    .map(res =>  res.json())
   }
 
   saveTable(table){
-
-      var headers = new Headers();
-      headers.append('Content-Type', 'application/json');
-
-      let post = this.http.post(this.basePath + '/table/', JSON.stringify(table),  {headers: headers})
-                             .map(res =>  res.json())
-                             .subscribe(data => { });
-      //post.unsubscribe();
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    let post = this.http.post(this.BO.tablesPOST, JSON.stringify(table),  {headers: headers})
+                        .map(res =>  res.json())
+                        .subscribe(data => { });
   }
 
   private handleError(error: any): Promise<any> {
-      console.error('An error occurred', error); // for demo purposes only
-      return Promise.reject(error.message || error);
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 }
