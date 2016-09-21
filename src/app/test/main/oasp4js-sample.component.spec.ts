87d28315baa1c6f7bf8c22c22bc758700589a37f
Belda Perez <cristobal.belda-perez@capgemini.com>
2016-09-21 17:30:01 +0200

    merge with new structure

--- src/app/test/main/oasp4js-sample.component.spec.ts
+++ src/app/test/main/oasp4js-sample.component.spec.ts
@@ -1,3 +1,33 @@
+// import { TestBed, async } from '@angular/core/testing'
+// import { Oasp4jsSampleAppComponent } from '../../main/views/main/oasp4js-sample.component'
+//
+// describe('App: Angular200OaspRev', () => {
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [
+//         Oasp4jsSampleAppComponent
+//       ],
+//     })
+//   })
+//
+//   it('should create the app', async(() => {
+//     let fixture = TestBed.createComponent(Oasp4jsSampleAppComponent)
+//     let app = fixture.debugElement.componentInstance;
+//     expect(app).toBeTruthy();
+//   }));
+//
+//   it(`should have as title 'app works!'`, async(() => {
+//     let fixture = TestBed.createComponent(Oasp4jsSampleAppComponent)
+//     let app = fixture.debugElement.componentInstance;
+//     expect(app.title).toEqual('oasp4js-sample works!')
+//   }))
+//
+// });
+//
+//
+//
+
+
 // import {
 //   beforeEachProviders,
 //   describe,
