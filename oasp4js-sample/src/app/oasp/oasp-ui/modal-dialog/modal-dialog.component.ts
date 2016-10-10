589611178d1340ac0296cc12aa144e5af9b2f89d
Garcia Marin <roberto.garcia-marin@capgemini.com>
2016-10-10 17:04:30 +0200

    table-grid test2 and modal-dialog test + code cleaning

--- oasp4js-sample/src/app/oasp/oasp-ui/modal-dialog/modal-dialog.component.ts
+++ oasp4js-sample/src/app/oasp/oasp-ui/modal-dialog/modal-dialog.component.ts
@@ -6,31 +6,8 @@ import { Component, Input } from '@angular/core';
 })
 export class ModalDialogComponent {
 
-  @Input('header') header: Object;
   @Input('title') title: string;
-  @Input('modal') modal: boolean;
-
-  defaultHeader: Object = '';
-  defaultBody: Object = 'OASP dialog body';
-
-  defaultButtonDefs: Object;
-  defaultTitle: String = 'OASP dialog title';
-
+  
   constructor() {};
 
-  getHeader() {
-      if (this.header != null) {
-        return this.header;
-      } else {
-        return this.defaultHeader;
-      }
-  }
-
-  getTitle() {
-      if (this.title != null) {
-        return this.title;
-      } else {
-        return this.defaultTitle;
-      }
-  }
 }
--- oasp4js-sample/src/app/test/oasp/oasp-ui/modal-dialog/modal-dialog.component.spec.ts
+++ oasp4js-sample/src/app/test/oasp/oasp-ui/modal-dialog/modal-dialog.component.spec.ts
@@ -1,10 +1,39 @@
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
 import { ModalDialogComponent } from '../../../../oasp/oasp-ui/modal-dialog/modal-dialog.component';
 
-let modalDialog: ModalDialogComponent;
+let comp: ModalDialogComponent;
+let fixture: ComponentFixture<ModalDialogComponent>;
+let Del: DebugElement;
+let htmlEl: HTMLElement;
 
-describe('modal-dialog-component', () => {
+describe('ModalDialogComponent', () => {
+
+  beforeEach( async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ModalDialogComponent ],
+    }).compileComponents();
+  }));
+
   beforeEach(() => {
-    modalDialog = new ModalDialogComponent();
+    fixture = TestBed.createComponent(ModalDialogComponent);
+    comp = fixture.componentInstance;
+
+    comp.title = 'modal-title';
+    fixture.detectChanges();
+
+    Del  = fixture.debugElement.query(By.css('h3'));
+    htmlEl = Del.nativeElement;
+
   });
 
+  it('Title has been defined', () => {
+    expect(htmlEl.textContent).toBeTruthy();
+  });
+
+  it('Input title should be equal to modal title', () => {
+    expect(htmlEl.textContent).toEqual(comp.title);
+  });
 });
