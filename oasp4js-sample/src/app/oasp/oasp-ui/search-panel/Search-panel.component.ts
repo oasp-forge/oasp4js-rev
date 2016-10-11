44370c5266576a9a7be970c99e116bf4327f66a6
Garcia Marin <roberto.garcia-marin@capgemini.com>
2016-10-11 11:33:29 +0200

    search-panel tests

--- oasp4js-sample/src/app/oasp/oasp-ui/search-panel/Search-panel.component.ts
+++ oasp4js-sample/src/app/oasp/oasp-ui/search-panel/Search-panel.component.ts
@@ -1,39 +1,30 @@
-import {Component, Input, Output, EventEmitter, OnChanges} from '@angular/core';
-import {OaspI18n} from '../../oasp-i18n/oasp-i18n.service';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { OaspI18n } from '../../oasp-i18n/oasp-i18n.service';
 
 @Component({
   selector: 'app-search-panel',
   templateUrl: 'Search-panel.component.html'
 })
 
-export class SearchPanelComponent implements OnChanges {
+export class SearchPanelComponent {
   @Input('searchInputs') searchInputs;
   @Output('searchValues') searchValues = new EventEmitter();
-  search;
+  search = [];
   i18n;
 
   constructor(oaspI18n: OaspI18n) {
       this.i18n = oaspI18n.getI18n();
   }
 
-  ngOnChanges() {
-      if (this.searchInputs) {
-          this.search = this.searchInputs;
-      }
-  }
-
   clearForm() {
-    for (let i = 0 ; i < this.search.length ; i++ ) {
-        this.search[i].value = '';
+    for (let i = 0 ; i < this.searchInputs.length ; i++ ) {
+        this.searchInputs[i].value = '';
     }
     this.doSearch();
   }
 
   doSearch() {
-      let values = [];
-      for (let i = 0 ; i < this.search.length ; i++ ) {
-          values.push(this.search[i].value);
-      }
-      this.searchValues.emit(values);
+      this.searchInputs.forEach(element => this.search.push(element.value));
+      this.searchValues.emit(this.search);
   }
 }
--- oasp4js-sample/src/app/test/oasp/oasp-ui/search-panel/search-panel.component.spec.ts
+++ oasp4js-sample/src/app/test/oasp/oasp-ui/search-panel/search-panel.component.spec.ts
@@ -1,11 +1,61 @@
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
 import { SearchPanelComponent } from '../../../../oasp/oasp-ui/search-panel/Search-panel.component';
 import { OaspI18n} from '../../../../oasp/oasp-i18n/oasp-i18n.service';
 
-let searchPanel: SearchPanelComponent;
+let comp: SearchPanelComponent;
+let fixture: ComponentFixture<SearchPanelComponent>;
+let DelSearch, DelCancel: DebugElement;
+let htmlElSearch, htmlElCancel: HTMLElement;
 
-describe('search-panel-component', () => {
+let oaspI18n, i18n;
+
+describe('SearchPanelComponent', () => {
+
+  beforeEach( async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SearchPanelComponent ],
+      providers: [ OaspI18n ]
+    }).compileComponents();
+  }));
+
   beforeEach(() => {
-    searchPanel = new SearchPanelComponent(new OaspI18n);
+      fixture = TestBed.createComponent(SearchPanelComponent);
+      comp = fixture.componentInstance;
+
+      oaspI18n = new OaspI18n();
+      i18n = oaspI18n.getI18n();
+
+      comp.searchInputs = [{name: 'id', value: '1'}, {name: 'state', value: 'FREE'}, {name: 'id', value: '1'}];
+      fixture.detectChanges();
+
+      DelSearch  = fixture.debugElement.query(By.css('.btn-primary'));
+      htmlElSearch = DelSearch.nativeElement;
+
+      DelCancel  = fixture.debugElement.query(By.css('.btn-warning'));
+      htmlElCancel = DelCancel.nativeElement;
+  });
+
+  it('buttons should have correctly translated text', () => {
+      expect(htmlElSearch.textContent).toEqual(i18n.buttons.searchBtn);
+  });
+
+  it('buttons should have correctly translated text', () => {
+      expect(htmlElCancel.textContent).toEqual(i18n.buttons.clearBtn);
+  });
+
+  it('Output values should be correctly builded', () => {
+      let searchValues;
+      comp.searchValues.subscribe((select) => searchValues = select);
+
+      comp.search = [];
+      comp.searchInputs.forEach(element => comp.search.push(element.value));
+      fixture.detectChanges();
+
+      DelSearch.triggerEventHandler('click', null);
+      expect(searchValues).toBe(comp.search);
   });
 
 });
